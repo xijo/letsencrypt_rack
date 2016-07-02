a8a59f5539d7da49cc9304548e9a72f359ccc6e5
Johannes Opper <johannes.opper@gmail.com>
2016-07-02 08:55:45 +0200

    Remove deprecation warning: "Passing strings or symbols to the middleware builder is deprecated"

--- lib/letsencrypt_rack/railtie.rb
+++ lib/letsencrypt_rack/railtie.rb
@@ -1,7 +1,7 @@
 module LetsencryptRack
   class Railtie < Rails::Railtie
     initializer "letsencrypt_rack.insert_middleware" do |app|
-      app.config.middleware.use "LetsencryptRack::Middleware"
+      app.config.middleware.use LetsencryptRack::Middleware
     end
   end
 end
--- lib/letsencrypt_rack/version.rb
+++ lib/letsencrypt_rack/version.rb
@@ -1,3 +1,3 @@
 module LetsencryptRack
-  VERSION = "0.1.0"
+  VERSION = "0.1.1"
 end
